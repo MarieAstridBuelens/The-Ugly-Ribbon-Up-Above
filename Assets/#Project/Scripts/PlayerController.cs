e801c8c7e59cf5d7c534253a586501a1749dc711
Marie-Astrid Buelens <mabuelens@gmail.com>
2024-11-04 15:53:14 +0100

    Controls and Move OK

--- Assets/#Project/Scripts/PlayerController.cs
+++ Assets/#Project/Scripts/PlayerController.cs
@@ -9,10 +9,16 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] InputActionAsset inputActions;
-    [SerializeField] private float speed = 2f;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float turnSmoothVelocity;
+    [SerializeField] private Transform cam;
+
     //InputAction run;
     InputAction move;
+    InputAction sideSteps;
     private NavMeshAgent agent;
+    Keyboard keyboard = Keyboard.current;
 
     void OnEnable(){
         inputActions.FindActionMap("PlayerMap").Enable();
@@ -21,6 +27,7 @@ public class PlayerController : MonoBehaviour
         // run.canceled += OnSpaceRelease;
 
         move = inputActions["Move"];
+        sideSteps = inputActions["SideSteps"];
     }
 
     void OnDisable(){
@@ -39,6 +46,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MoveControls();
+        SideControls();
         // //arrêt de la course
         // Keyboard keyboard = Keyboard.current;
         // if (keyboard.shiftKey.wasReleasedThisFrame){
@@ -57,12 +65,30 @@ public class PlayerController : MonoBehaviour
 
     
     void MoveControls(){
-        Keyboard keyboard = Keyboard.current;
+        
         Vector3 moveAmount = move.ReadValue<Vector3>();
         Debug.Log(moveAmount);
 
-        transform.position += new Vector3(moveAmount.x, moveAmount.y, moveAmount.z) * Time.deltaTime * speed;
+        if(moveAmount == Vector3.zero) return; // SINONla direction indiquée va quand même donner un vectuer qui initie un mouvement !
 
+        //rotation du Player vers sa direction
+        //Atan2 = fonction qui retourne l'angle en radiants entre axe x et le vecteur allant de (0,0) à (moveAmount.x, moveAmount.y)
+        //Mathf.Rad2Deg : traduit radiants en degrés
+        //Quaternion.Euler nous permet de donner trois nombre, la rotation autour de x, y et z.
+        float targetAngle = Mathf.Atan2(moveAmount.x, moveAmount.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        
+        //assouplir la rotation du Player
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        //donne la direction vers laquelle on veut aller en prenant en compte l'angle de la caméra
+        Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        transform.position += new Vector3(moveDirection.x, moveDirection.y, moveDirection.z).normalized * Time.deltaTime * speed;
+    }
+
+    void SideControls(){
+        Vector3 sideAmount = sideSteps.ReadValue<Vector3>();
+        transform.position += new Vector3(sideAmount.x, sideAmount.y, sideAmount.z).normalized * Time.deltaTime * speed;
     }
 
     // void OnSpace(InputAction.CallbackContext context){
