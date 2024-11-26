eb4570539a8ba8663db261dcb03f8f2c195b1aed
Marie-Astrid Buelens <mabuelens@gmail.com>
2024-11-26 15:04:05 +0100

    untraversable bow ok, change scene ok

--- Assets/#Project/Scripts/SceneChanger.cs
+++ Assets/#Project/Scripts/SceneChanger.cs
@@ -1,33 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
 
-    
-    float time = 5f;
-    [SerializeField] float delay = 5f; //variable delay
-    private string sceneName;
-
-    void Start(){
-        time = delay;
-    }
-    
-    void Update(){ //sert à mettre un délai avant le chargement de la scène, uniquement après que la fonction Change ait été appelée.
-        if(time < delay){
-            time += Time.deltaTime;
-            if(time >= delay){
-                SceneManager.LoadScene(sceneName);
-            }
-        }
-    }
-    
-    public void Change(string sceneName){
-        sceneName = sceneName.Trim();
-        this.sceneName = sceneName;
-        time = 0f;
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            SceneManager.LoadScene(1); 
+        } 
     }
 
+    // float time = 5f;
+    // [SerializeField] float delay = 5f; //variable delay
+    // [SerializeField] private string sceneName;
+    // [SerializeField] private UnityEvent whenLevelFinished;
+    // private bool counterOn = false;
+
+    // void Start(){
+    //     time = delay;
+    // }
+    
+
+    // void OnTriggerEnter(Collider other){
+    //     if(other.tag == "Player"){
+    //         counterOn = true;
+    //         //Debug.Log("triggered");
+    //     } 
+    // }
+
+    // void Update(){ //sert à mettre un délai avant le chargement de la scène, uniquement après que la fonction Change ait été appelée.
+    //     if (counterOn){
+    //         if(time < delay){
+    //             time += Time.deltaTime;
+
+    //             if(time >= delay){
+    //                 sceneName = sceneName.Trim();
+    //                 time = 0f;
+    //                 counterOn = false;
+    //                 SceneManager.LoadScene(sceneName); 
+    //                 this.sceneName = sceneName;
+    //             }
+    //         }
+    //     }
+    // }
 }
